8fab7b4ba6d265bdf749d8f011afa63bde5c8dc5
evillarv <esthervillarv@gmail.com>
2023-01-23 17:31:01 -0500

    update pom.xml - checkstyle and reporting configuration

--- ms-BankAccount/src/main/java/com/nttdata/bootcamp/ms/bankaccount/repository/BankAccountRepository.java
+++ ms-BankAccount/src/main/java/com/nttdata/bootcamp/ms/bankaccount/repository/BankAccountRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BankAccountRepository extends ReactiveMongoRepository<BankAccount,Integer> {
+public interface BankAccountRepository extends ReactiveMongoRepository<BankAccount, Integer> {
 
 }
